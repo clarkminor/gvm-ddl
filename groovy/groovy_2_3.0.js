d7b5908b5c3ce4d3d2ac7287f0de3d7d73605512
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-05-20 14:58:23 +0100

    Adds groovy 2.3.1, fixes removal of 2.3.0.

--- groovy/groovy_2_3.0.js
+++ groovy/groovy_2_3.0.js
@@ -1,8 +1,2 @@
 //push new version
 db.candidates.update({candidate:"groovy"}, {"$push": {versions: {version: "2.3.0", url: "http://dl.bintray.com/groovy/maven/groovy-binary-2.3.0.zip"}}});
-
-//pull old version
-db.candidates.update({candidate:"groovy"}, {$pull: {versions: {version: "2.3.0-rc-4"}}});
-
-//update default
-db.candidates.update({candidate:"groovy"}, {$set :{default:"2.3.0"}});
