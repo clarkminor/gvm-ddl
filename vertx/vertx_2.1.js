22144c8e8409508ef19f8c4919bb11f7b64f2214
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-05-28 09:33:49 +0100

    Adds vertx 2.1 final.

---
+++ vertx/vertx_2.1.js
@@ -0,0 +1,8 @@
+//push new version
+db.candidates.update({candidate:"vertx"}, {"$push": {versions: {version: "2.1", url: "http://dl.bintray.com/vertx/downloads/vert.x-2.1.zip"}}});
+
+//update default
+db.candidates.update({candidate:"vertx"}, {$set :{default:"2.1"}});
+
+//pull an entry from an array
+db.candidates.update({candidate:"vertx"}, {$pull: {versions: {version: "2.1RC3"}}});
