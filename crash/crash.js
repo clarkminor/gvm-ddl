452b6ab62aeac478b3e8dc2613e9fb9c86f395df
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-06-10 13:00:32 +0100

    Adds crash as new candidate.

---
+++ crash/crash.js
@@ -0,0 +1,7 @@
+db.candidates.insert(
+	{candidate:"crash", default:"1.2.11", versions:[
+			{version: "1.2.11", url: "http://dl.bintray.com/crashub/crash/crash-1.2.11.zip"},
+			{version: "1.3.0.RC7", url: "http://dl.bintray.com/crashub/crash/crash-1.3.0.RC7.zip"}
+		]
+	}
+);
