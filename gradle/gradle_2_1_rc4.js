07f205d76e6a0345ffca995232f48063923688b0
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-09-04 17:33:40 +0100

    Adds gradle 2.1-rc-4

---
+++ gradle/gradle_2_1_rc4.js
@@ -0,0 +1,5 @@
+//pull an entry from an array
+db.candidates.update({candidate:"gradle"}, {$pull: {versions: {version: "2.1-rc-2"}}});
+
+//push new version
+db.candidates.update({candidate:"gradle"}, {"$push": {versions: {version: "2.1-rc-4", url: "http://services.gradle.org/distributions/gradle-2.1-rc-4-all.zip"}}});
