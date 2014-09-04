8e314b030bec1c55f3d87ca6a79e3aaeed8bf32b
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-09-04 17:26:21 +0100

    Adds groovy 2.4.0-beta-3

---
+++ groovy/groovy_2_4.0-beta-3.js
@@ -0,0 +1,5 @@
+//push new version
+db.candidates.update({candidate:"groovy"}, {"$push": {versions: {version: "2.4.0-beta-3", url: "http://dl.bintray.com/groovy/maven/groovy-binary-2.4.0-beta-3.zip"}}});
+
+//pull old version
+db.candidates.update({candidate:"groovy"}, {$pull: {versions: {version: "2.4.0-beta-2"}}});
