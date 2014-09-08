fec90feb6a47e881712d477c0fca158ddd3672d9
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-09-08 06:47:57 -0500

    Adds gradle 2.1 rc 4

--- gradle/gradle_2_1_rc4.js
+++ gradle/gradle_2_1_rc4.js
@@ -1,4 +1,5 @@
 //pull an entry from an array
+db.candidates.update({candidate:"gradle"}, {$pull: {versions: {version: "2.1-rc-1"}}});
 db.candidates.update({candidate:"gradle"}, {$pull: {versions: {version: "2.1-rc-2"}}});
 
 //push new version
